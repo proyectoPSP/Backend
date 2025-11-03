e63cbce9dcaebd24ea43437fbb30ad6018937854
josema <jgonsed2808@g.educaand.es>
2025-11-03 10:28:45 +0100

    Commit inicial

---
+++ src/main/java/com/proyectopsp/proyectopsp/ProyectoPspApplication.java
@@ -0,0 +1,13 @@
+package com.proyectopsp.proyectopsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoPspApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proyectopsp/proyectopsp/ProyectoPspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectopsp.proyectopsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPspApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
