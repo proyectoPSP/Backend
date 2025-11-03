3a7e6dcd17d3967686c399e5700aec0d9bf56280
ElRojos236 <rgoncam0107@g.educaand.es>
2025-11-03 12:29:29 +0100

    Imports para la ia

--- src/main/java/com/proyectopsp/proyectopsp/ai/IAService.java
+++ src/main/java/com/proyectopsp/proyectopsp/ai/IAService.java
@@ -1,4 +1,12 @@
 package com.proyectopsp.proyectopsp.ai;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+@Service
 public class IAService {
 }
